d4718d1e6fe8e724b895a1e383af8c2cf1f3e0c0
joeySchentrup <j03y@live.com>
2025-01-03 15:00:47 +0000

    Add description to edit page view

--- woocommerce-multi-signup/src/edit.js
+++ woocommerce-multi-signup/src/edit.js
@@ -11,17 +11,16 @@ export const Edit = ({ attributes, setAttributes }) => {
     const blockProps = useBlockProps();
     return (
         <div {...blockProps}>
-            <div className={'example-fields'}>
-                <ValidatedTextInput
-                    id="student_data"
-                    type="text"
-                    required={false}
-                    className={'student_data'}
-                    label={
-                        'Student Data example'
-                    }
-                    value={''}
-                />
+            <div style={{ padding: "20px", border: "1px solid #ddd", borderRadius: "4px" }}>
+                <h3
+                    style={{ margin: "0 0 12px 0", fontSize: "1em" }}
+                    class={"wc-block-components-title"}
+                >
+                    Student Info
+                </h3>
+                <p>
+                    This is where the student inputs will go. It will appear if the user tries to buy more than one of the products.
+                </p>
             </div>
         </div>
     );
